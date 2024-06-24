2562a0a04a3ee55103b69b2a2dab3a1a9a36fbdf
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-06-24 09:03:22 -0500

    Write first preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrositetemplate.netlify.app/",
+  site: "https://astrositetemplate.netlify.app/",
+  integrations: [preact()]
 });
